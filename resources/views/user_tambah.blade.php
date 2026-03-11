c7612cc34315544bf1250184c50b5d6fceebd0b6
Esatovin Ebenhaezer Victoria <esatovinv@gmail.com>
2026-03-11 20:52:51 +0700

    Jobsheet4_Praktikum2.6

---
+++ resources/views/user_tambah.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tambah</title>
+</head>
+<body>
+    <h1>Form Tambah Data User</h1>
+        <form action="{{ url('/user/tambah_simpan') }}" method="post">    
+        {{ csrf_field() }}
+
+        <label>Username</label>
+        <input type="text" name="username" placeholder="Masukkan Username">
+        <br>
+        <label>Nama</label>
+        <input type="text" name="nama" placeholder="Masukkan Nama">
+        <label>password</label>
+        <input type="password" name="password" placeholder="Masukkan password">
+        <br>
+        <label>Level Pengguna</label>
+        <input type="number" name="level_id" placeholder="Masukkan ID Level">
+        <br><br>
+        <input type="submit" class="btn btn-success" value="simpan"></input>
+    </form>
+</body>
+</html>
